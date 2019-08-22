5a3639040267ca25baba28c2e43510fb84e3f0a4
William White <willcwhite@gmail.com>
2019-08-22 17:55:55 -0400

    included routing in server.js

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
 
 
 
